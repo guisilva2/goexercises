56a30a36932af070b29f9c9276c59b6fc23d7478
Guilherme Silva <guilhermesilva.fe@gmail.com>
2018-02-09 19:49:50 -0200

    Commit

--- teste.go
+++ teste.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Teste2")
+	aPoints, bPoints := 0, 0
+	a, b := make([]int, 3), make([]int, 3)
+	fmt.Scan(&a[0], &a[1], &a[2])
+	fmt.Scan(&b[0], &b[1], &b[2])
+	for i, v := range a {
+		if v > b[i] {
+			aPoints++
+		} else if v < b[i] {
+			bPoints++
+		}
+	}
+	fmt.Print(aPoints, bPoints)
 }
