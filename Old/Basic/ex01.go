3118253103afc764f80a576fd5288560e6a662c6
Guilherme Ferreira da Silva <36019822+guisilvadevel@users.noreply.github.com>
2021-01-20 21:13:59 -0300

    Delete Old directory

--- Old/Basic/ex01.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("Olá, exemplo 01!")
-}
--- Old/Basic/ex02.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import "time"
-
-func main() {
-	fmt.Println("Bem-vindo ao Playground!")
-	fmt.Println("A hora agora é", time.Now())
-}
